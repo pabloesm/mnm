fcb2f636feff89d7cea4654023ecec9ae9d033d2
pabloem <pabloe85@gmail.com>
2022-12-18 20:05:54 +0100

    Fix fstrings

--- scrapper/run_node_script.py
+++ scrapper/run_node_script.py
@@ -5,9 +5,9 @@ def run_node_script(url_full: str, url_domain: str, news_id: int) -> None:
     command = [
         "node",
         "scripts/node/commentsElconfidencial.js",
-        f"--url_full=${url_full}",
-        f"--url_domain=${url_domain}",
-        f"--news_id=${news_id}",
+        f"--url_full={url_full}",
+        f"--url_domain={url_domain}",
+        f"--news_id={news_id}",
     ]
     # https://stackoverflow.com/a/28319191/3782161
     with Popen(command, stdout=PIPE, bufsize=1, universal_newlines=True) as process:
